e6b82c8c88d3901e0756549d1b3a76d07eea76ac
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-26 12:36:16 +0530

    Add Assignment 3 Files

---
+++ Assignment_3/code01.py
@@ -0,0 +1,8 @@
+# Wap to calculate area of rectangle taking input from user
+
+l = int(input("Enter the length = "))
+w = int(input("Enter the width = "))
+
+area = l*w 
+
+print("The area of rectangle is = ", area)
---
+++ Assignment_3/code02.py
@@ -0,0 +1,8 @@
+# Wap to add one integer and floating type number taking input from user
+
+num1 = int(input("Enter the number in Integer = "))
+num2 = float(input("Enter the number in Float = "))
+
+sum = float(num1 + num2)
+
+print("The sum of two numbers is = ", sum)
