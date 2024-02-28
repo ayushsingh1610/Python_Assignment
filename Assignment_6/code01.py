3b319e92a56d12715c3ff2f885e8a4248a061cbd
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-28 21:59:14 +0530

    Create code01

---
+++ Assignment_6/code01.py
@@ -0,0 +1,7 @@
+# Write a program to read 2 numbers from the user . Display the result using bitwise & ,|,^ operator on the numbers.
+num1 = int(input("Enter the 1st number = "))
+num2 = int(input("Enter the 2nd number = "))
+
+print("Bitwise AND result = ", num1&num2)
+print("Bitwise OR result = ", num1|num2)
+print("Bitwise XOR result = ", num1^num2)
