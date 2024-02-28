39c5533d642a87eba415ae863608af13aace469b
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-28 22:45:05 +0530

    Create code02.py

---
+++ Assignment_6/code02.py
@@ -0,0 +1,6 @@
+# Write a program to shift input data by 2 bits towards the right.
+num = int(input("Enter the number = "))
+
+print("Value before shifting towards right = ", num)
+
+print("Value after shifting towards right = ", num>>2)
