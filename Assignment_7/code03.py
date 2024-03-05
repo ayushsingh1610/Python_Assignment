3b853d9fa9d1e8c2b2216d666455d3c7af80078d
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-05 23:45:01 +0530

    Create code03.py

---
+++ Assignment_7/code03.py
@@ -0,0 +1,12 @@
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+n = int(input("Enter the last number = "))
+even =0
+odd =0
+for i in range(1, n+1):
+    if(i%2==0):
+        even+=1
+    else:
+        odd+=1
+
+print("Even number in giver range = ", even)
+print("ODD number in giver range = ", odd)
