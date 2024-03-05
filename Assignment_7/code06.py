2e113bac87c17dde3e399617dd6cdef2b8adfb71
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-05 23:45:12 +0530

    Create code06.py

---
+++ Assignment_7/code06.py
@@ -0,0 +1,6 @@
+# Write a program using the not operator. 
+age = int(input("Enter your age = "))
+if(not age<18):
+    print("You are Eligible to vote")
+else:
+    print("You are NOT Eligible to vote")
