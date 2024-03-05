b200fa447267a50bbf2a922c8ede46d90de19277
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-05 23:45:17 +0530

    Create code08.py

---
+++ Assignment_7/code08.py
@@ -0,0 +1,11 @@
+# Write a Python program to print alphabet pattern P 
+for i in range(1,5):
+    for j in range(1,5):
+        if(i==1 or i==4 or j==1 or j==4):
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    
+    print()
+for i in range(1,5):
+    print("*")
