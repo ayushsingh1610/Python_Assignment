7865d348ef57948598f650d8a792f6bc5787dad9
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-08 19:53:39 +0530

    update

--- Assignment_5/code01.py
+++ Assignment_5/code01.py
@@ -1,2 +1,4 @@
+# Write a program to print 'A' to 'F' in different lines.
+
 for ch in range(65, 71):
     print(chr(ch))
--- Assignment_5/code07.py
+++ Assignment_5/code07.py
@@ -1,4 +1,10 @@
-Pounds = 10
-kilogram = float(Pounds*0.45)
+"""Translate the following algorithm into Python code.
+    Step 1: Initialize variable named Pounds with value 10.
+    Step 2: Multiply Pounds by 0.45 and assign it to a variable Kilogram.
+    Step 3: Display the value of variable Pounds and Variable.
+    """
+    
+pounds = 10
+kilogram = float(pounds*0.45)
 
 print(kilogram)
