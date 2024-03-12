ba7a395404e40662c0fff7be9ae1cbcbb9c3de37
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-12 22:59:56 +0530

    Update code14

--- Assignment_10/code14.py
+++ Assignment_10/code14.py
@@ -1,1 +1,12 @@
 # Write a program using variable length arguments to calculate the average of numbers till the values given in function.
+
+def func_average(*args):
+    sum = 0
+    for i in args:
+        sum+=i
+        
+    return sum/len(args)
+
+print("Average of all numbers is = ", func_average(2,13,23,24,25,67,45,23))
+    
+    
